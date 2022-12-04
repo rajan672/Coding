595a0b20dad1aeeefa153fb35ac867dc17f3ddfb
Rajan Sah <76938099+rajan672@users.noreply.github.com>
2022-12-04 17:47:43 +0530

    merge

---
+++ 6.Sorting/4_Merge_sort.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void merge(int *,int, int , int );
+void merge_sort(int *arr, int low, int high)
+{
+    int mid;
+    if (low < high){
+        //divide the array at mid and sort independently using merge sort
+        mid=(low+high)/2;
+        merge_sort(arr,low,mid);
+        merge_sort(arr,mid+1,high);
+        //merge or conquer sorted arrays
+        merge(arr,low,high,mid);
+    }
+}
+// Merge sort 
+void merge(int *arr, int low, int high, int mid)
+{
+    int i, j, k, c[50];
+    i = low;
+    k = low;
+    j = mid + 1;
+    while (i <= mid && j <= high) {
+        if (arr[i] < arr[j]) {
+            c[k] = arr[i];
+            k++;
+            i++;
+        }
+        else  {
+            c[k] = arr[j];
+            k++;
+            j++;
+        }
+    }
+    while (i <= mid) {
+        c[k] = arr[i];
+        k++;
+        i++;
+    }
+    while (j <= high) {
+        c[k] = arr[j];
+        k++;
+        j++;
+    }
+    for (i = low; i < k; i++)  {
+        arr[i] = c[i];
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    merge_sort(arr, 0, n-1);
+    cout<<"Sorted array\n";
+
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+
+}
